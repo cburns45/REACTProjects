316da7436205403291843e7e552299658384c7cf
cburns45 <connor.burns19@gmail.com>
2022-01-19 14:36:26 -0500

    changes

--- pokemon/src/components/Pokemon/index.js
+++ pokemon/src/components/Pokemon/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import react from 'react';
+
 
 class Pokemon extends React.Component{
     
--- pokemon/src/components/Search/index.js
+++ pokemon/src/components/Search/index.js
@@ -1,8 +1,8 @@
-import Axios from "axios";
+import axios from "axios";
 import { useState } from "react";
-import Pokemon from "../Pokemon";
+//import Pokemon from "../Pokemon";
 
-function Search(){
+/* function Search(){
     const [search, setSearch] = useState ('');
 
     const [pokemon, setPokemon] = useState ({});
@@ -46,7 +46,7 @@ function Search(){
                                 <p>Loading...</p>
                             </div>
                         ) : (
-                            <div>
+                            <div className="border">
                                 <Pokemon name = {pokemon.name}
                                 id = {pokemon.id}
                                 sprites = {pokemon.sprites.front_default} />
@@ -58,5 +58,84 @@ function Search(){
             
         );
 }
+
+export default Search; */
+
+function Search (){
+    const [pokemon, setPokemon] = useState("pikachu");
+    const [pokemonData, setPokemonData] = useState ([]);
+    const [pokemonType, setPokemonType] = useState ("");
+
+    const handleChange = (e) => {
+        setPokemon(e.target.value.toLowerCase());
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getPokemon();
+    };
+
+    const getPokemon = async () => {
+        const toArray = [];
+
+        try {
+            const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+            const res = await axios.get(url);
+            toArray.push(res.data);
+            setPokemonType(res.data.types[0].type.name);
+            setPokemonData(toArray);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    console.log(pokemonData);
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input type="text" onChange={handleChange}
+                    placeholder = "search pokemon"/>
+                </label>
+            </form>
+
+            {pokemonData.map((data) => {
+                return (
+                    <div className="container">
+                        <img src= {data.sprites["front_default"]} />
+                        <div className="divTable">
+                            <div className="divTableBody">
+                                <div className="divTableRow">
+                                    <div className="divTableCell">Type</div>
+                                    <div className="divTableCell">{pokemonType}</div>
+                                </div>
+                                <div className="divTableRow">
+                                    <div className="divTableCell">Height</div>
+                                    <div className="divTableCell">
+                                        {""}
+                                        {Math.round(data.height * 3.9)}
+                                    </div>
+                                </div>
+                                <div className="divTableRow">
+                                    <div className="divTableCell">Weight</div>
+                                    <div className="divTableCell">
+                                        {""}
+                                        {Math.round(data.weight / 4.3)}
+                                    </div>
+                                </div>
+                                <div className="divTableRow">
+                                    <div className="divTableCell">Number of Battles</div>
+                                    <div className="divTableCell">{data.game_indices.length}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
 
 export default Search;
